5b35ee5c1dfcfe714fe958e1e3ae48aa6aa2325d
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-01-29 23:26:45 +0530

    Added: new file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollDice
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int Diceroll = 0;
+            Random random = new Random();
+            int number = random.Next(0, 7);
+            Console.WriteLine("Number of dice rolls " + Diceroll);
+            Console.WriteLine(number);
+        }
+    }
+}

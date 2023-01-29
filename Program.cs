2d73ac105b6410ec5c2f0154c25236827fb0089d
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-01-29 16:51:43 +0530

    Added: new file

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnaksandLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Random random = new Random();
+            int number = random.Next(0,2);
+            Console.WriteLine(number);
+            if (position < 100)
+            {
+                position = 0;
+                Console.WriteLine("the position is" + position);
+            }
+            else
+            {
+                Console.WriteLine("No Player" + position);
+            }
+        }
+    }
+}
